2ab53d11918d24cca38fb85787a4088c6dcf1c56
Burke Holland <burkeholland@gmail.com>
2022-11-03 15:07:52 +0000

    Get more specific

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Welcome to React in a Codespace!
+          Welcome to React in a Codespace! A small change
         </a>
       </header>
     </div>
